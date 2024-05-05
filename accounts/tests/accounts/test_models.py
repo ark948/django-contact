2f72f1c2b6d9f1444b0c3709f9e721b07657b0cc
ark948 <armankarimi948@gmail.com>
2024-05-05 19:17:10 +0330

    Added login test

--- accounts/tests/accounts/test_models.py
+++ accounts/tests/accounts/test_models.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from accounts.models import CustomUser
+from django.contrib.auth import authenticate
 
 class AccountsCustomUserModel(TestCase):
     def setUp(self) -> None:
-        self.user = CustomUser()
-        self.user.username = "some_username"
+        self.user = CustomUser.objects.create(username="some_username")
         self.user.first_name = "firstName"
         self.user.last_name = "lastName"
         self.user.email = "somerandom@email.com"
         self.user.full_name = self.user.first_name + " " + self.user.last_name
-        self.user.id = 1
+        self.user.pk = 1
+        self.user.set_password("123")
+        self.user.save() # this is necessary
         return super().setUp()
     
     def test_user_info(self):
@@ -18,5 +20,11 @@ class AccountsCustomUserModel(TestCase):
         self.assertEqual(self.user.last_name, "lastName")
         self.assertEqual(self.user.email, "somerandom@email.com")
         self.assertEqual(self.user.full_name, "firstName lastName")
-        self.assertEqual(self.user.id, 1)
+        self.assertEqual(self.user.pk, 1)
+        self.assertEqual(self.user.check_password("123"), True)
+
+    def test_user_auth(self):
+        logged_in = self.client.login(username="some_username", password="123")
+        self.assertTrue(logged_in)
+    
         
--- pages/tests/pages/test_views.py
+++ pages/tests/pages/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse
+from accounts.models import CustomUser
 
 # simpletestcase -> since home page does not need database
 # Create your tests here.
@@ -14,3 +15,13 @@ class HomePageTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/home.html")
         self.assertContains(response, "صفحه اصلی")
+
+class TestUserLogin(TestCase):
+    def setUp(self) -> None:
+        self.user = CustomUser()
+        self.user.pk = 1
+        return super().setUp()
+    
+    def test_user_was_created(self):
+        self.assertEqual(isinstance(self.user, CustomUser), True)
+        self.assertEqual(self.user.pk, 1)
